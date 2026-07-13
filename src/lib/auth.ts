02144dfd7741de56eb6874fe0f1ae11960a2e398
Sani-Mohibur <mohiburrahmansani@gmail.com>
2026-07-13 08:54:27 +0600

    fix: resolve base url

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,7 +10,7 @@ export const auth = betterAuth({
     provider: "postgresql",
   }),
 
-  baseURL: process.env.BACKEND_URL,
+  baseURL: process.env.BETTER_AUTH_URL,
 
   emailAndPassword: {
     enabled: true,
